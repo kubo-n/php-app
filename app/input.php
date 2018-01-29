34b5342b4026a683151ac50149fcd1c53d847da9
Nao Kubo <fewsbhds@gmail.com>
2018-01-29 14:18:27 +0900

    commitしました。

--- app/input.php
+++ app/input.php
@@ -23,15 +23,21 @@
      }  
     //登録ページ遷移処理
     function transition(){
-
-    // 「OK」時の処理 ＋ 確認ダイアログの表示
-    if(window.confirm('登録してもよろしいですか？')){
-        location.href = "entry.php"; // 遷移
-    }
-    // 「キャンセル」時の処理
-    else{
-     //   window.alert('キャンセルされました'); // 警告ダイアログを表示
-    }
+    //入力チェック
+        if (document.getElementById('title').value == "" ){
+            alert('タイトルが未入力です。');
+        }else if(document.getElementById('amount').value == "" ){
+            alert('何人分かが未入力です。');
+        }else if(document.getElementById('ingredients').value == "" ){
+            alert('材料が未入力です。');
+        }else if(document.getElementById('recipe1').value == "" ){
+            alert('レシピが未入力です。');        
+        }else{
+            // 「OK」時の処理 ＋ 確認ダイアログの表示
+            if(window.confirm('登録してもよろしいですか？')){
+                location.href = "entry.php"; // 遷移
+            }
+        }
     }
 </script> 
 
@@ -52,57 +58,59 @@
         <hr width="500">
         <div align="left">
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        タイトル&emsp;<input type="text" value="" name="title" size="48"><br><br>
+        <font size="2">※タイトル、分量(何人分)、材料、レシピは必須項目です。</font><br><br>          
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        材料(<input type="text" value="" name="title" size="3">)人分<br>
+        タイトル&emsp;<input type="text" value="" name="title" id="title" size="48"><br><br>
+        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+        材料(<input type="text" value="" name="amount" id="amount" size="3">)人分<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="5" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="ingredients" id="ingredients" rows="5" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         レシピ<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         1<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe1" id="recipe1" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         2<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe2" id="recipe2" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         3<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe3" id="recipe3" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         4<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe4" id="recipe4" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         5<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe5" id="recipe5" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         6<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe6" id="recipe6" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         7<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe7" id="recipe7" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         8<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe8" id="recipe8" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         9<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe9" id="recipe9" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         10<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe10" id="recipe10" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         その他メモ<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="5" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="memo" id="memo" rows="5" cols="50" wrap="hard"></textarea><br>
         <form id="my_form">
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         写真選択&emsp;
--- app/read.php
+++ app/read.php
@@ -7,15 +7,10 @@
 <script type="text/javascript">
     //削除ページ遷移処理
     function transition(){
-
-    // 「OK」時の処理 ＋ 確認ダイアログの表示
-    if(window.confirm('削除してもよろしいですか？')){
-        location.href = "delete.php"; // 遷移
-    }
-    // 「キャンセル」時の処理
-    else{
-    //    window.alert('キャンセルされました'); // 警告ダイアログを表示
-    }
+        // 「OK」時の処理 ＋ 確認ダイアログの表示
+        if(window.confirm('削除してもよろしいですか？')){
+            location.href = "delete.php"; // 遷移
+        }
     }
 </script> 
 
@@ -31,57 +26,57 @@
         <hr width="500">
         <div align="left">
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        タイトル&emsp;<input type="text" value="" name="title" size="48"><br><br>
+        タイトル&emsp;<input type="text" value="" name="title" id="title" size="48"><br><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        材料(<input type="text" value="" name="title" size="3">)人分<br>
+        材料(<input type="text" value="" name="amount" id="amount" size="3">)人分<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="5" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="ingredients" id="ingredients" rows="5" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         レシピ<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         1<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe1" id="recipe1" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         2<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe2" id="recipe2" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         3<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe3" id="recipe3" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         4<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe4" id="recipe4" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         5<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe5" id="recipe5" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         6<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe6" id="recipe6" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         7<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe7" id="recipe7" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         8<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe8" id="recipe8" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         9<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe9" id="recipe9" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         10<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="3" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="recipe10" id="recipe10" rows="3" cols="50" wrap="hard"></textarea><br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         その他メモ<br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-        <textarea name="テキストエリア" rows="5" cols="50" wrap="hard"></textarea><br>
+        <textarea value="" name="memo" id="memo" rows="5" cols="50" wrap="hard"></textarea><br>
         <br>
         &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
         <a href="list.php">戻る</a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
